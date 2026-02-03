2c4a40336f60bd2eb6293d0085389b05a10e6c52
BenchPress200 <jms0538@naver.com>
2026-02-03 21:30:13 +0900

    init

---
+++ src/main/java/com/benchpress200/searchsyncprocessor/SearchSyncProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.benchpress200.searchsyncprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchSyncProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchSyncProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/benchpress200/searchsyncprocessor/SearchSyncProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benchpress200.searchsyncprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchSyncProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
