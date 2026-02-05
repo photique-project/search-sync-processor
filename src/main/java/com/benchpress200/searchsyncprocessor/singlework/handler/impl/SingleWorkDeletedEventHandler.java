a08267ebdf1c42a6dcb58f0509f77067a0e00ab7
BenchPress200 <jms0538@naver.com>
2026-02-06 00:00:06 +0900

    feat: 단일작품 삭제 이벤트 처리 핸들러 추가

---
+++ src/main/java/com/benchpress200/searchsyncprocessor/singlework/handler/impl/SingleWorkDeletedEventHandler.java
@@ -0,0 +1,27 @@
+package com.benchpress200.searchsyncprocessor.singlework.handler.impl;
+
+import com.benchpress200.searchsyncprocessor.common.constant.EventType;
+import com.benchpress200.searchsyncprocessor.singlework.consumer.payload.SingleWorkEventPayload;
+import com.benchpress200.searchsyncprocessor.singlework.handler.SingleWorkEventHandler;
+import com.benchpress200.searchsyncprocessor.singlework.repository.SingleWorkSearchRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SingleWorkDeletedEventHandler implements SingleWorkEventHandler {
+    private final SingleWorkSearchRepository singleWorkSearchRepository;
+
+    @Override
+    public String getEventType() {
+        return EventType.DELETED;
+    }
+
+    @Override
+    public void handle(Long eventId, SingleWorkEventPayload payload) {
+        Long singleWorkId = payload.getId();
+
+        singleWorkSearchRepository.findById(singleWorkId)
+                .ifPresent(singleWorkSearchRepository::delete);
+    }
+}
